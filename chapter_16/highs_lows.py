16bf4a4f0e0ac0f0d8de52cc970a5806abb1a614
“Pangxi” <you@example.com>
2021-11-16 18:11:46 +0800

    logs

--- chapter_16/highs_lows.py
+++ chapter_16/highs_lows.py
@@ -4,19 +4,24 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # 从文件中获取日期、最高气温和最低气温
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
 
-        high = int(row[1])
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
+            dates.append(current_date)
+            highs.append(high)
+
+            lows.append(low)
 
     # 根据数据绘制图形
     fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -25,7 +30,8 @@ with open(filename) as f:
     # 传递两个y值，填两个数据集之间的区域着色
     plt.fill_between(dates,highs,lows,facecolor='blue', alpha=0.1)
     # 设置图形的格式
-    plt.title("Daily high and low temperatures - 2014", fontsize=24)
+    title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+    plt.title(title, fontsize=24)
     plt.xlabel('', fontsize=16)
     # 斜的日期标签
     fig.autofmt_xdate()
