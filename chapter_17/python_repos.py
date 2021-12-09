0666eb19bb473fa866473596beb348356005fae4
“Pangxi” <you@example.com>
2021-12-09 17:38:09 +0800

    logs

--- chapter_17/python_repos.py
+++ chapter_17/python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 
 
 # 执行API调用并存储响应
@@ -12,19 +15,48 @@ print("Total repositories:",response_dict['total_count'])
 
 # 研究有关仓库的信息
 repo_dicts = response_dict['items']
+print("Number of items:", len(repo_dicts))
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+    }
+
+    plot_dicts.append(plot_dict)
+
+    # 可视化
+    my_style = LS('#333366', base_style=LCS)
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.title_font_size = 24
+    my_config.label_font_size = 14
+    my_config.major_label_font_size = 18
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+    chart = pygal.Bar(my_config, style=my_style)
+    chart.title = 'Most-Starred Python Projects on GitHub'
+    chart.x_labels = names
+
+    chart.add('', plot_dicts)
+    chart.render_to_file('python_repos.svg')
+
+"""
 print("Repositories returned:",len(repo_dicts))
 # 研究第一个仓库
-repo_dict = repo_dicts[0]
 print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
 
-
+"""
 
 
 
